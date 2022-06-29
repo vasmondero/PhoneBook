b7ecdf0bd5e220d74cd8caba56f4bf0867f1b392
vasmondero <72551147+vasmondero@users.noreply.github.com>
2022-06-29 14:53:35 +0200

    Initial commit

---
+++ PhoneBook/PhoneBook.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhoneBook
+{
+    class PhoneBook
+    {
+        public List<Contact> Contacts { get; set; } = new List<Contact>();
+
+        public void AddContact(Contact contact)
+        {
+            Contacts.Add(contact);
+        }
+
+        private void DisplayContactDetails(Contact contact)
+        {
+            Console.WriteLine($"Contact: {contact.Name}, {contact.Number}");
+        }
+
+
+        private void DisplayContactsDetails(List<Contact> contacts)
+        {
+            foreach (var contact in contacts)
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+
+        public void DisplayContact(string number)
+        {
+            var contact = Contacts.FirstOrDefault(contact => contact.Number == number);
+
+            if (contact == null)
+            {
+                Console.WriteLine("Contact not found");
+            }
+            else
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+
+        public void DisplayAllContacts()
+        {
+            DisplayContactsDetails(Contacts);
+        }
+        public void DisplayMatchingContacts(string serchPhrase)
+        {
+            var machingContacts = Contacts.Where(c => c.Name.Contains(serchPhrase)).ToList();
+            DisplayContactsDetails(machingContacts);           
+        }
+    }
+}
